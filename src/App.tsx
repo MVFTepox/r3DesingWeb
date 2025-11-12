e4d3cff72a217ebf93c1d3027e4b583423ddfe95
Mario Fraire <fraire.tepox.mario.valentin@gmail.comgit config --global user.email fraire.tepox.mario.valentin@gmail.com>
2025-11-11 21:31:36 -0600

    hola

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="comingsoon-container">
+      <div className="blur-bg"></div>
+      <div className="content">
+        <div className="logo">R3Desing</div>
+        <h1>Próximamente</h1>
+        <p>
+          Estamos creando algo asombroso para ti. 💡
+          <br /> ¡Vuelve pronto y descúbrelo!
+        </p>
+        <div className="loader"></div>
+      </div>
+      <footer>© 2025 R3Desing. Todos los derechos reservados.</footer>
+    </div>
+  );
+}
